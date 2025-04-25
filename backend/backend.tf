2205b8c06ad758574fb64bc031b57a43d52f4972
Naresh Narayanadasu <101800735+NareshNarayanadasu@users.noreply.github.com>
2025-04-25 17:03:50 +0530

    Update backend.tf

--- backend/backend.tf
+++ backend/backend.tf
@@ -2,6 +2,6 @@ terraform {
    backend "gcs" {
      bucket  = "michaels-tf-state"   # Replace with your GCS bucket name
      prefix  = "envs/dev" 
-     credentials = "${BUCKET_SA}"          # Separate prefix for each env
+     credentials = "${env.BUCKET_SA}"          # Separate prefix for each env
    }
  }
